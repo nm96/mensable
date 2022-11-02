c1ed57912424bb382c60b3d03d6b21f3791ef066
Nico <nicholasmarrin@gmail.com>
2022-11-02 15:01:32 +0000

    Create ultra-basic app.py with minimal imports

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
