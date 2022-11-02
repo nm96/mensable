c803d430fcd34a78faff284415543c7b7814f193
Nico <nicholasmarrin@gmail.com>
2022-11-02 15:30:31 +0000

    Make html auto-reload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
 
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 Session(app)
 
