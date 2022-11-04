d885536ee51ee996a7033298b9820f651004a145
Nico <nicholasmarrin@gmail.com>
2022-11-04 12:11:18 +0000

    Create separate models and helpers modules

---
+++ models.py
@@ -0,0 +1,11 @@
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
