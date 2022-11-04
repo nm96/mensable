d664255141f0ec2bc9b0749f649355e391872285
Nico <nicholasmarrin@gmail.com>
2022-11-04 11:42:36 +0000

    Tidy up templates, store session cookies on filesystem

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 app.secret_key = "abcdefg"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Set up database
 db = SQLAlchemy(app)
