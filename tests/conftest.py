e50f2030d575568d21655f50c7630e90a095f2ce
Nico <nicholasmarrin@gmail.com>
2022-12-05 17:04:54 +0000

    Fix quotes in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,24 +6,14 @@ from mensable.models import *
 
 @pytest.fixture
 def app():
-    app = create_app({'TESTING': True,
-        'SQLALCHEMY_DATABASE_URI': "sqlite:///:memory:"})
+    app = create_app({"TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"})
 
     with app.app_context():
         # Add dummy user to temporary test database
         user = User("testuser", generate_password_hash("testpwd"))
         db.session.add(user)
         db.session.commit()
-        # Add dummy language
-        #language = Language('Testese')
-        #db.session.add(language)
-        # Add a dummy table
-        # TODO: Use the actual API route here rather than trying to reproduce
-        # the functionality!
-        #table = Table('Testtable')
-        #table.creator_id = 1
-        #table.language_id = 1
-        #db.session.add(table)
 
     return app
 
@@ -47,11 +37,11 @@ class AuthActions(object):
             "password": password, "confirmation": confirmation})
 
     def login(self, username=username, password=password):
-        return self._client.post( '/login', data={'username': username,
-            'password': password})
+        return self._client.post( "/login", data={"username": username,
+            "password": password})
 
     def logout(self):
-        return self._client.get('/logout')
+        return self._client.get("/logout")
 
 
 @pytest.fixture
