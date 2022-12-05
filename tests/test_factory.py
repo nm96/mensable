87df40f4720a9f39cb9d0633f11d330b518ed818
Nico <nicholasmarrin@gmail.com>
2022-12-05 17:05:35 +0000

    Fix quotes in tests

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -2,5 +2,5 @@ from mensable import create_app
 
 def test_config():
     assert not create_app().testing
-    assert create_app({'TESTING': True, 
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'}).testing
+    assert create_app({"TESTING": True, 
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"}).testing
