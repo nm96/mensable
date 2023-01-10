9a8cce9d4cca209629de0d81521401ce9873d7c8
Nico <nicholasmarrin@gmail.com>
2023-01-10 16:26:50 +0000

    Set database uri in factory test

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,6 +1,13 @@
+import os    
+
 from mensable import create_app
 
 def test_config():
+    # Artificially set database url environment variable.
+    uri = "sqlite:///:memory:"
+    os.environ["DATABASE_URL"] = uri
+    # Create app in normal mode.
     assert not create_app().testing
+    # Create app in testing mode.
     assert create_app({"TESTING": True, 
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"}).testing
+        "SQLALCHEMY_DATABASE_URI": uri}).testing
